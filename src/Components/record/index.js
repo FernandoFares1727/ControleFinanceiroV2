4894596b9512a3f8994cceb9986528aac66c7a3b
FernandoFares1727 <fernandimfares@gmail.com>
2024-07-29 11:38:21 -0300

    Notations flow

--- src/Components/record/index.js
+++ src/Components/record/index.js
@@ -1,20 +1,149 @@
 import './style.css';
-import { useState } from 'react';
+import { useState, useRef } from 'react';
+import Notation from './notation/index.js';
 
 const Record = (props) => {
 
     const [title, setTitle] = useState(props.title)
     const [subtitle, setSubtitle] = useState(props.subtitle)
 
+    const titleRef = useRef(null)
+    const subtitleRef = useRef(null)
+
+    const onTitleChanged = () => {
+        if (titleRef.current) {
+            const newTitle = titleRef.current.innerText
+            setTitle(newTitle)
+            props.onTitleChange(props.id, newTitle)
+        }
+    }
+
+    const onSubtitleChanged = () => {
+        if (subtitleRef.current){
+            const newSubtitle = subtitleRef.current.innerText
+            setSubtitle(newSubtitle)
+            props.onSubtitleChange(props.id, newSubtitle)
+        }
+    }
+
+    const [notationsEnabled, setNotationsEnabled] = useState(false)
+
+    const notationsIcon = () => {
+        return notationsEnabled 
+            ? props.upIcon
+            : props.downIcon
+    }
+
+    const showNotations = () => {
+        setNotationsEnabled(!notationsEnabled)
+    }
+
+    const [notations, setNotations] = useState([])
+
+    const addNotation = () => {
+        setNotations([...notations, { id:props.createGuid() ,date: Date.now(), description: "Descrição", value: 0 }])
+    }
+        
+    const removeNotation = (id) => {
+        const notation = notations.find(notation => notation.id === id)
+        setNotations(notations.filter(notation => notation.id !== id))
+        setTotalValue(totalValue - notation.value)
+    }
+        
+    const sortedNotations = [...notations].sort((a, b) => b.date - a.date)
+
+    // Calculate the total value efficiently using a reducer function
+    const calculateTotalValue = () => {
+        const total = notations.reduce((acc, notation) => acc + parseFloat(notation.value), 0)
+        return total 
+    }
+
+    const [totalValue,setTotalValue] = useState(calculateTotalValue())
+
+    const valueColor = (value) => {
+        if (value == 0)
+            return "#000000"
+        else if (value < 0)
+            return "#FF033E"
+        return "#6D9406"
+    }
+
+    const handleNotationDescriptionChange = (id, newDescription) => {
+        const updatedNotations = notations.map(notation => {
+            if (notation.id === id){
+                notation.description = newDescription
+                return { ...notation} 
+            }
+            else {
+                return notation
+            }
+        })
+        setNotations(updatedNotations)
+        props.onNotationChange(id, updatedNotations)
+    }
+
+    const handleNotationValueChange = (id,newValue) => {
+        const updatedNotations = notations.map(notation => {
+          if (notation.id === id) { // Check if it's the updated notation
+            notation.value = newValue
+            return { ...notation} // Update the value property
+          } else {
+            return notation
+          }
+        })
+        setNotations(updatedNotations)
+        setTotalValue(calculateTotalValue(updatedNotations)) // Recalculate total value
+        props.onNotationChange(updatedNotations)
+    }      
+
     return (
         <div className="Record">
-            <div className='Record-Description'>
-                <span onChange={(event) => setTitle(event.target.value)} className='Titulo' contentEditable={true}>{title}</span>
-                <span onChange={(event) => setSubtitle(event.target.value)} className='Subtitulo' contentEditable={true}>{subtitle}</span>
+            <div className='Record-Header'>
+                <div className='Record-Description'>
+                    <span ref={titleRef} onBlur={onTitleChanged} className='Titulo' contentEditable={true}>{title}</span>
+                    <span ref={subtitleRef} onBlur={onSubtitleChanged} className='Subtitulo' contentEditable={true}>{subtitle}</span>
+                </div>
+                <button onClick={props.onDelete}>
+                    <img src={props.deleteIcon} alt='delete'/>
+                </button>
             </div>
-            <button onClick={props.onDelete}>
-                <img src={props.deleteIcon} alt='delete'/>
+            <button onClick={showNotations} className='Record-Notations'>
+                <img src={notationsIcon()} alt='Show Notations'/>
             </button>
+            {notationsEnabled && (
+                <div className='Notation-Container'>
+                    <hr />
+                    <div onClick={addNotation} className='Add-Notation-Conatiner'>
+                        <button className='Add-Notation'>
+                            <img src={props.addNotation} alt='Add Notation'/>
+                        </button>
+                    </div>
+                    {sortedNotations.length > 0 && ( // Check if sortedNotations has elements
+                    <div className="Notation-Total-Value">
+                        <img src={props.money} alt='Money'/>
+                        <span style={{color: valueColor(totalValue)}}>{totalValue}</span>
+                    </div>
+                    )}
+                    <div className='Notation-Body'>
+                    {sortedNotations.map(notation => (
+                      <Notation
+                        key={notation.date}
+                        id={notation.id}
+                        description= {notation.description}
+                        value={notation.value}
+                        delete={props.deleteIcon}
+                        money={props.money}
+                        valueColor={valueColor}
+                        onDelete={() => removeNotation(notation.id)}
+                        onDescriptionChange={(id, newDescription) => handleNotationDescriptionChange(id, newDescription)}
+                        onValueChange={(id, newValue) => handleNotationValueChange(id, newValue)}
+                      />
+                    ))
+                    }
+                    </div>
+                </div>
+            )
+            }
         </div>
     );
 }
---
+++ src/Components/record/notation/index.js
@@ -0,0 +1,40 @@
+import { useState,useRef } from 'react'
+import './style.css'
+
+const Notation = (props) => {
+
+    const [description, setDescription] = useState(props.description)
+    const [value, setValue] = useState(props.value)
+
+    const descriptionRef = useRef(null)
+    const valueRef = useRef(null)
+
+    const onDescriptionChanged = () => {
+        if (descriptionRef.current) {
+            const newDescription = descriptionRef.current.innerText
+            setDescription(newDescription)
+            props.onDescriptionChange(props.id, newDescription)
+        }
+    }
+
+    const onValueChanged = () => {
+        if (valueRef.current) {
+            const newValue = valueRef.current.innerText
+            setValue(newValue)
+            props.onValueChange(props.id, newValue)
+        }
+    }
+
+    return(<div className='Notation'>
+        <span contentEditable={true} ref={descriptionRef} className='Notation-Description' onBlur={onDescriptionChanged}>{description}</span>
+        <div>
+            <img src={props.money} alt='Money'/>
+            <span style={{color: props.valueColor(value)}} contentEditable={true} ref={valueRef} className='Notation-Value' onBlur={onValueChanged}>{value}</span>
+            <button onClick={props.onDelete} className='Notation-Delete'>
+                <img src={props.delete} alt='Notation-Delete'/>
+            </button>
+        </div>
+    </div>)
+}
+
+export default Notation
